86853cfa7bbc1b8582f356e0d14b2a6684ce6990
arpi <arpipatel190407@gmail.com>
2026-08-14 09:04:03 +0530

    Initial React Practical2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import React from "react";
+import Student from "./Student";
 
 function App() {
 return (
 <div>
-<h1>Welcome to React</h1>
-<h2>My First React Application</h2>
-<p>
-This is a simple React application created using JSX and functional
-components.
-</p>
-<p>React makes it easy to build user interfaces.</p>
+<h1>Student Details</h1>
+
+<Student
+name="Rahul Kumar"
+rollNo="101"
+course="BCA"
+marks="85%"
+/>
+
+<Student
+name="Priya Singh"
+rollNo="102"
+course="BCA"
+marks="92%"
+/>
+
+<Student
+name="Amit Sharma"
+rollNo="103"
+course="BCA"
+
+marks="78%"
+/>
 </div>
 );
 }
